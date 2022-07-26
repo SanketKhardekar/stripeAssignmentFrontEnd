3deb247e36de4b4f80273797ca122e725034b102
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-07-26 20:10:55 +0530

    Created constants file for comman data

---
+++ src/constants/constant.js
@@ -0,0 +1,9 @@
+export const product_image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS09jU-7hq2QtNT0bEE4ES_n_fjmluh4qAQpQ&usqp=CAU";
+export const navItems=["Laptops","Desktop PCs","Networking Devices","Printer & Scanners","PC Parts","All Other Products","Reports" ];
+
+export const productImages=[
+    "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV1?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861333000",
+    "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV3?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861336000",
+    "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV5_GEO_IN?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1633987141000",
+
+]
