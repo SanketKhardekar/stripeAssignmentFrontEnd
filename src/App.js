7aea3adc8c3d09063dae966babbd548545fa3f0f
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-07-26 20:11:24 +0530

    Updated the file added Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from "react";
+import "./App.css";
+import { productImages } from "./constants/constant";
+import Appbar from "./components/Appbar";
+import NavbarComponent from "./components/NavbarComponent";
+import { Container, Grid } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade,Autoplay,Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 
-function App() {
+export default function App() {
+  // const [message, setMessage] = useState("");
+
+  // useEffect(() => {
+  //   // Check to see if this is a redirect back from Checkout
+  //   const query = new URLSearchParams(window.location.search);
+
+  //   if (query.get("success")) {
+  //     setMessage("Order placed! You will receive an email confirmation.");
+  //   }
+
+  //   if (query.get("canceled")) {
+  //     setMessage(
+  //       "Order canceled -- continue to shop around and checkout when you're ready."
+  //     );
+  //   }
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Fragment>
+      <Appbar />
+      <NavbarComponent />
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ marginTop: -1 }}
+      >
+        <Grid
+          item
+          xs={10}
+          sm={10}
+          md={8}
+          lg={6}
+          sx={{ backgroundColor: "#f8f8ff" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Container maxWidth="xs">
+            sdfdsifsdj;ofisdofjpasdiopgjsdauio
+          </Container>
+        </Grid>
+        <Grid item xs={10} sm={10} md={8} lg={5}>
+          <Container maxWidth="sm">
+          <Swiper
+            spaceBetween={50}
+            effect={"fade"}
+            pagination={{
+              clickable: false,
+            }}
+            autoplay={{
+              delay: 900,
+              disableOnInteraction: true,
+            }}
+            modules={[EffectFade,Autoplay,Pagination]}
+          >
+            {productImages.map((item, index) => {
+              return(
+              <SwiperSlide key={index}>
+                <img src={item} alt="product_images" />
+              </SwiperSlide>);
+            })}
+          </Swiper>
+          </Container>
+        </Grid>
+      </Grid>
+    </Fragment>
   );
 }
-
-export default App;
