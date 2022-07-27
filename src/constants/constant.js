e1e14320a0db66749f261097f922998c5d2023e2
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-07-27 22:01:51 +0530

    Added Product Description

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,9 +1,31 @@
-export const product_image_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS09jU-7hq2QtNT0bEE4ES_n_fjmluh4qAQpQ&usqp=CAU";
-export const navItems=["Laptops","Desktop PCs","Networking Devices","Printer & Scanners","PC Parts","All Other Products","Reports" ];
+export const product_image_url =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS09jU-7hq2QtNT0bEE4ES_n_fjmluh4qAQpQ&usqp=CAU";
+export const navItems = [
+  "Laptops",
+  "Desktop PCs",
+  "Networking Devices",
+  "Printer & Scanners",
+  "PC Parts",
+  "All Other Products",
+  "Reports",
+];
 
-export const productImages=[
-    "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV1?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861333000",
-    "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV3?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861336000",
-    "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV5_GEO_IN?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1633987141000",
+export const productImages = [
+  "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV1?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861333000",
+  "https://rukminim1.flixcart.com/image/416/416/kwdv3bk0/headphone/5/9/2/-original-imag92pgraqqcg8u.jpeg?q=70",
+  "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV3?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1632861336000",
+  "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MME73_AV5_GEO_IN?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1633987141000",
+];
 
-]
+export const product_description = [
+  "Spatial audio with dynamic head tracking places sound all around you",
+  "Adaptive EQ automatically tunes music to your ears",
+  "All-new contoured design",
+  "Force sensor lets you easily control your entertainment, space answer or end calls and more",
+  "Sweat and water resistent",
+  "Up to 6 hrs of listening time with one charge",
+  "Up to 30 hrs of listening time with the MagSafe Charging Case",
+  `Quick access to Siri by saying "Hey Siri"`,
+  "Effortless setup, in-ear detection and automactic switching for magical experience",
+  "Easily share audio between two sets of AirPods on your iPhone, iPad, iPad-touch or Apple TV"
+];
