4caa11ba18f779d1fc3e6cec1e19c1f75fe1a824
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-07-27 22:03:19 +0530

    Added Product Description

---
+++ src/components/ProductDescription.jsx
@@ -0,0 +1,90 @@
+import { Box, Typography } from "@mui/material";
+import { Fragment } from "react";
+import { product_description } from "../constants/constant";
+
+const ProductDescription = (props) => {
+  return (
+    <Fragment>
+      <Box sx={{ paddingY: { md: 12, xs: 10 } }}>
+        <Typography
+          sx={{
+            display: { xs: "none", md: "flex" },
+            marginBottom: { xs: 0, md: 3 },
+          }}
+          color="#A2A6B0"
+          fontSize="12px"
+          fontWeight="normal"
+          fontFamily="Poppins, sans-serif"
+          component="section"
+          gutterBottom
+        >
+          APPLE PRODUCTS
+        </Typography>
+        <Typography
+          sx={{
+            textDecorationLine: "underline",
+            textDecorationColor: "blue",
+            textUnderlineOffset: "0.3rem",
+            fontWeight: 600,
+            display: { xs: "flex", md: "none" },
+            paddingBottom: 3,
+          }}
+          align="left"
+          fontSize="12px"
+          color="#666666"
+          fontFamily="Poppins, sans-serif"
+          component="section"
+        >
+          About Product
+        </Typography>
+        <Typography
+          align="left"
+          sx={{ fontWeight: 500 }}
+          fontSize={{ md: "32px", xs: "22px" }}
+          fontFamily="Poppins, sans-serif"
+          component="section"
+          gutterBottom
+        >
+          New Apple AirPods (3rd Generation)
+        </Typography>
+        <Typography
+          sx={{ display: { lg: "block", xs: "none" } }}
+          align="left"
+          fontSize="12px"
+          fontFamily="poppins, sans-serif"
+          component="section"
+          gutterBottom
+        >
+          It's magic, remastered!
+        </Typography>
+        <ul>
+          {product_description.map((item, index) => {
+            return (
+              <li key={index}>
+                <Typography
+                  align="left"
+                  fontSize="12px"
+                  fontFamily="Poppins, sans-serif"
+                  component="section"
+                  gutterBottom
+                >
+                  {item}
+                </Typography>
+              </li>
+            );
+          })}
+        </ul>
+        <Typography
+          align="left"
+          fontSize="12px"
+          fontFamily="Poppins, sans-serif"
+          component="section"
+          gutterBottom
+        >
+          SKU D5515AI
+        </Typography>
+      </Box>
+    </Fragment>
+  );
+};
+export default ProductDescription;
